d00d8cf77156ebc4eba291c26e487576fc4d9574
Elton Silva <eltoncarsilva73@gmail.com>
2026-02-17 19:59:56 -0300

    created Intentory class

---
+++ Project/Entities/InventoryGameObject.cs
@@ -0,0 +1,20 @@
+﻿using UmbrellaToolsKit;
+
+namespace Project.Entities
+{
+    public class InventoryGameObject : GameObject
+    {
+        public static InventoryGameObject Instance;
+
+        public override void Start()
+        {
+            Instance = this;
+        }
+
+        public override void OnDestroy()
+        {
+            Instance = null;
+            base.OnDestroy();
+        }
+    }
+}
