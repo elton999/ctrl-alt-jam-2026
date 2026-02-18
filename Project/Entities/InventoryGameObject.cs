05ae8c5634d509d0ef36fb2a1694d31d8f9cb7c7
Elton Silva <eltoncarsilva73@gmail.com>
2026-02-17 21:44:00 -0300

    added logic to use item from inventory

--- Project/Entities/InventoryGameObject.cs
+++ Project/Entities/InventoryGameObject.cs
@@ -1,10 +1,19 @@
-﻿using UmbrellaToolsKit;
+﻿using System.Collections.Generic;
+using UmbrellaToolsKit;
 
 namespace Project.Entities
 {
     public class InventoryGameObject : GameObject
     {
         public static InventoryGameObject Instance;
+
+        public Dictionary<ToolsTypes, int> Tools = new Dictionary<ToolsTypes, int>()
+        { 
+            { ToolsTypes.AXE, 0 },
+            { ToolsTypes.BOMB, 0 },
+            { ToolsTypes.SWORD, 0 }, 
+            { ToolsTypes.BOOT, 0 } 
+        };
 
         public override void Start()
         {
@@ -15,6 +24,26 @@ namespace Project.Entities
         {
             Instance = null;
             base.OnDestroy();
+        }
+
+        public static bool HasItem(ToolsTypes tool)
+        {
+            if (Instance == null) return false;
+            if (Instance.Tools[tool] > 0) return true;
+
+            return false;
+        }
+
+        public static bool UseItem(ToolsTypes tool)
+        {
+            if (Instance == null) return false;
+            if (HasItem(tool))
+            {
+                Instance.Tools[tool] -= 1;
+                return true;
+            }
+
+            return false;
         }
     }
 }
