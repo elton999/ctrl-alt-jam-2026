fc021369c25d8302c2e696e09139610efa969d88
Elton Silva <eltoncarsilva73@gmail.com>
2026-02-19 20:04:08 -0300

    created level manager entity

---
+++ Project/Entities/LevelManagerEntity.cs
@@ -0,0 +1,22 @@
+﻿using UmbrellaToolsKit;
+
+namespace Project.Entities
+{
+    public class LevelManagerEntity : GameObject
+    {
+        public static LevelManagerEntity Instance;
+
+        public override void Start()
+        {
+            if(Instance == null)
+            {
+                Instance = new LevelManagerEntity();
+            }
+        }
+
+        public override void OnDestroy()
+        {
+            Instance = null;
+        }
+    }
+}
