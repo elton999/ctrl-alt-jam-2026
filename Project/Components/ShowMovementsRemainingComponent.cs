6546b250e20e58e97f6a06a15adeacd7299f5369
Elton Silva <eltoncarsilva73@gmail.com>
2026-02-23 20:17:53 -0300

    logic to update remaing movements

---
+++ Project/Components/ShowMovementsRemainingComponent.cs
@@ -0,0 +1,20 @@
+﻿using Project.Entities;
+using UmbrellaToolsKit;
+
+namespace Project.Components
+{
+    public class ShowMovementsRemainingComponent : Component
+    {
+        private UITextComponent _textComponent;
+
+        public override void Start()
+        {
+            _textComponent = GameObject.GetComponent<UITextComponent>();
+        }
+
+        public override void Update(float deltaTime)
+        {
+            _textComponent.SetText(LevelManagerEntity.RemainingMovements.ToString());
+        }
+    }
+}
