8fa9a2fa0d10789d09d2935365b69ee9054b8942
Elton Silva <eltoncarsilva73@gmail.com>
2026-02-23 07:59:20 -0300

    integration of hud animation

--- Project/Entities/UI/UILevelManagerEntity.cs
+++ Project/Entities/UI/UILevelManagerEntity.cs
@@ -15,19 +15,25 @@ namespace Project.Entities.UI
             tag = nameof(UILevelManagerEntity);
 
             _hudBoard = new GameObject();
+            _hudBoard.tag = "hud board";
             Scene.AddGameObject(_hudBoard, Layers.UI);
             _hudBoard.AddComponent<SpriteComponent>().SetAtlas("hud board");
             _hudBoard.AddComponent<HudLevelAnimation>();
 
             _countMovement = new GameObject();
+            _countMovement.tag = "buoy ui";
             Scene.AddGameObject(_countMovement  , Layers.UI);
             _countMovement.AddComponent<SpriteComponent>().SetAtlas("buoy ui");
-            _countMovement.AddComponent<HudLevelAnimation>().SetRenderPosition(HudLevelAnimation.RenderPosition.RIGHT);
+            var hudAnimation = _countMovement.AddComponent<HudLevelAnimation>();
+            hudAnimation.SetRenderPosition(HudLevelAnimation.RenderPosition.RIGHT);
+            hudAnimation.SetAnimationDuration(1.5f);
+            _countMovement.AddComponent<UIAnimationByPlayerMovementComponent>();
 
             _hudPortrait = new GameObject();
+            _hudPortrait.tag = "portrait hud";
             Scene.AddGameObject(_hudPortrait, Layers.UI);
             _hudPortrait.AddComponent<SpriteComponent>().SetAtlas("portrait hud");
-            _hudPortrait.AddComponent<HudLevelAnimation>();
+            _hudPortrait.AddComponent<HudLevelAnimation>().SetAnimationDuration(1.2f);
         }
     }
 }
