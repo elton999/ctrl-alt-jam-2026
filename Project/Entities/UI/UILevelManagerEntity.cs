992680939322779a845e097078637b505c2793d5
Elton Silva <eltoncarsilva73@gmail.com>
2026-02-22 21:14:34 -0300

    integration of the sprites from level HUD

--- Project/Entities/UI/UILevelManagerEntity.cs
+++ Project/Entities/UI/UILevelManagerEntity.cs
@@ -1,4 +1,5 @@
-﻿using UmbrellaToolsKit;
+﻿using Project.Components;
+using UmbrellaToolsKit;
 using UmbrellaToolsKit.Components.Sprite;
 
 namespace Project.Entities.UI
@@ -6,6 +7,8 @@ namespace Project.Entities.UI
     public class UILevelManagerEntity : GameObject
     {
         private GameObject _hudBoard;
+        private GameObject _countMovement;
+        private GameObject _hudPortrait;
 
         public override void Start()
         {
@@ -14,6 +17,17 @@ namespace Project.Entities.UI
             _hudBoard = new GameObject();
             Scene.AddGameObject(_hudBoard, Layers.UI);
             _hudBoard.AddComponent<SpriteComponent>().SetAtlas("hud board");
+            _hudBoard.AddComponent<HudLevelAnimation>();
+
+            _countMovement = new GameObject();
+            Scene.AddGameObject(_countMovement  , Layers.UI);
+            _countMovement.AddComponent<SpriteComponent>().SetAtlas("buoy ui");
+            _countMovement.AddComponent<HudLevelAnimation>().SetRenderPosition(HudLevelAnimation.RenderPosition.RIGHT);
+
+            _hudPortrait = new GameObject();
+            Scene.AddGameObject(_hudPortrait, Layers.UI);
+            _hudPortrait.AddComponent<SpriteComponent>().SetAtlas("portrait hud");
+            _hudPortrait.AddComponent<HudLevelAnimation>();
         }
     }
 }
