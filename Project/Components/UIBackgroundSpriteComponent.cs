3c110504c6f801a6fe831ec1549246d182b57992
Elton Silva <eltoncarsilva73@gmail.com>
2026-03-31 15:45:33 -0300

    created background sprite

---
+++ Project/Components/UIBackgroundSpriteComponent.cs
@@ -0,0 +1,17 @@
+﻿using UmbrellaToolsKit;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Project.Components
+{
+    public class UIBackgroundSpriteComponent : Component
+    {
+        public void SetSprite(Texture2D sprite, Color color)
+        {
+            GameObject.Sprite = sprite;
+            GameObject.SpriteColor = color;
+
+            GameObject.Scale = GameObject.Scene.Sizes.X; 
+        }
+    }
+}
