2d3bb17e2f0cdc7919b635f8ecba5c4b185f8f41
Elton Silva <eltoncarsilva73@gmail.com>
2026-04-30 22:16:21 -0300

    removed old usings in some classes

--- Libs/UmbrellaToolsKit/Components/Sprite/SpriteComponent.cs
+++ Libs/UmbrellaToolsKit/Components/Sprite/SpriteComponent.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using MonoGame.ImGui.Extensions;
 using UmbrellaToolsKit.EditorEngine;
 using UmbrellaToolsKit.EditorEngine.Attributes;
 using UmbrellaToolsKit.EditorEngine.GameSettings;
--- Project/Game1.cs
+++ Project/Game1.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Project.Entities;
 using Project.Entities.Obstacles;
