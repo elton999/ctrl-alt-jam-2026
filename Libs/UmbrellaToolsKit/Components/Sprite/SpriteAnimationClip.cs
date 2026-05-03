00d41fe47f2cd7810911262064db0db6bd178209
Elton Silva <eltoncarsilva73@gmail.com>
2026-05-03 00:10:03 -0300

    created logic to set animation ina simple way

--- Libs/UmbrellaToolsKit/Components/Sprite/SpriteAnimationClip.cs
+++ Libs/UmbrellaToolsKit/Components/Sprite/SpriteAnimationClip.cs
@@ -7,5 +7,19 @@ namespace UmbrellaToolsKit.Components.Sprite
         public List<SpriteFrame> Frames = new();
         public string ClipName;
         public AnimationDirection AnimationDirection = AnimationDirection.FORWARD;
+
+        public SpriteAnimationClip(Sprite[] sprites, float durationByFrame, string name = "default")
+        {
+            foreach (var sprite in sprites)
+            {
+                Frames.Add(new SpriteFrame()
+                {
+                    Duration = durationByFrame,
+                    Sprite = sprite,
+                });
+
+                ClipName = name;
+            }
+        }
     }
 }
