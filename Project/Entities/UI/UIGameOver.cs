6f98b5c0012cb1d8d3580365a07148461fceca2d
Elton Silva <eltoncarsilva73@gmail.com>
2026-06-06 00:00:28 -0300

    integration of try again button visual

--- Project/Entities/UI/UIGameOver.cs
+++ Project/Entities/UI/UIGameOver.cs
@@ -15,6 +15,7 @@ namespace Project.Entities.UI
         {
             tag = "game over screen";
             LevelManagerEntity.OnLevelStateChanged += OnChangeLevelState;
+
         }
 
         public override void Destroy()
@@ -46,6 +47,15 @@ namespace Project.Entities.UI
             spriteComponent.OrigenToCenter();
             skull.Position = Scene.Sizes.ToVector2().Half().Truncate();
             skull.Position += Vector2.UnitY * -30f;
+
+            var tryAgainButton = new GameObject();
+            tryAgainButton.tag = "try again button";
+            Scene.AddGameObject(tryAgainButton, Layers.UI);
+            var tryAgainButtonSpriteComponent = tryAgainButton.AddComponent<SpriteComponent>();
+            tryAgainButtonSpriteComponent.SetAtlas("try again btn");
+            tryAgainButtonSpriteComponent.OrigenToCenter();
+            tryAgainButton.Position = Scene.Sizes.ToVector2().Half();
+            tryAgainButton.Position += Vector2.UnitY * 100.0f;
 
             yield return null;
         }
