20a70e8a2b6d4a23d3b20001c3341c51ce99a65f
Elton Silva <eltoncarsilva73@gmail.com>
2026-08-12 20:01:16 -0300

    added animation component on efx animation game object

--- Project/Entities/EfxAnimationGameObject.cs
+++ Project/Entities/EfxAnimationGameObject.cs
@@ -1,8 +1,29 @@
+using System.Collections.Generic;
 using UmbrellaToolsKit;
+using UmbrellaToolsKit.Components.Sprite;
 
 namespace Project.Entities
 {
     public class EfxAnimationGameObject : GameObject
     {
+        public enum AnimationType
+        {
+            HIT = 0,
+            SKULL = 1
+        }
+
+        private List<SpriteAnimationClip> _animationClips;
+        private AnimationComponent _animationComponent;
+
+        public override void Start()
+        {
+            _animationClips = new List<SpriteAnimationClip>();
+            _animationComponent = AddComponent<AnimationComponent>();
+        }
+
+        public void SetAnimation(AnimationType animationType)
+        {
+
+        }
     }
 }
